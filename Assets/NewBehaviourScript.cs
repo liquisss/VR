23861587202a64ceb013199c3220134030f4961b
liquisss <lebedevaliza96@gmail.com>
2023-09-21 15:12:34 +0300

    first commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Color origCol;
+    private Color hoverCol = Color.green;
+    private Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        origCol = rend.material.color; 
+    }
+    
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverCol;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = origCol;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
